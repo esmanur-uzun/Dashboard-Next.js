181fc4b00e2221b34b18789db78da5d6e47489f4
esmanur-uzun <esmabitmez@gmail.com>
2024-03-06 17:36:38 +0300

    worked on navbar

---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./header.module.css"
+
+const header = () => {
+  return (
+    <div className='h-14 bg-slate-800'>
+      
+    </div>
+  )
+}
+
+export default header
