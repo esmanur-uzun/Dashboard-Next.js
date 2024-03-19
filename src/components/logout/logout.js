1ca727b79901717e43f57cefd5e8eab0afa455f7
esmanur-uzun <esmabitmez@gmail.com>
2024-03-19 16:06:58 +0300

    worked on next auth

--- src/components/logout/logout.js
+++ src/components/logout/logout.js
@@ -1,19 +1,22 @@
 "use client"
-import React from 'react'
-import {store} from '@/store'
-import { fetchUser } from "@/store/auth";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { fetchUser } from '@/store/auth';
 
- function Logout  ()  {
-     store.dispatch(fetchUser())
-    function handleLogout (){
-        
-        console.log(user[0]);
-    }
+function Logout() {
+  const data = useSelector(state => state.auth.value)
+  const handleLogout = () => {
+    console.log(data); // Kullanıcı bilgisini consola yazdır
+    // Logout işlemi için gereken kodları buraya ekleyin
+  };
+
   return (
     <div>
-      <button onClick={handleLogout} className="ml-4">Logout</button>
+      <button onClick={handleLogout} className="ml-4">
+        Logout
+      </button>
     </div>
-  )
+  );
 }
 
-export default Logout
+export default Logout;
--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -3,7 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
   user: {},
 };
-
+export const fetchUser = () => async (dispatch) => {
+  try {
+    const response = await fetch("http://localhost:3001/posts");
+    const user = await response.json();
+    dispatch(actions._stateAcount(user)); // Yüklenen kullanıcıyı state'e yerleştir
+  } catch (error) {
+    // Hata durumunu işleyin
+  }
+};
 export const { reducer, actions } = createSlice({
   name: "auth",
   initialState,
@@ -17,12 +25,3 @@ export const { reducer, actions } = createSlice({
   },
 });
 
-export const fetchUser = () => async (dispatch) => {
-  try {
-    const response = await fetch("http://localhost:3001/posts");
-    const user = await response.json();
-    dispatch(actions._stateAcount(user)); // Yüklenen kullanıcıyı state'e yerleştir
-  } catch (error) {
-    // Hata durumunu işleyin
-  }
-};
