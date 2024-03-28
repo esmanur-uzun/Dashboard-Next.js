52f3eb1155bd9e987e7de4502127007c8ba2ba88
esmanur-uzun <esmabitmez@gmail.com>
2024-03-28 11:02:47 +0300

    completed sidebar design

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,20 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header  from "@/components/header/header";
-import  Sidebar  from "@/components/sidebar/sidebar";
+import Sidebar from "@/components/sidebar/sidebar";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
-import {store} from '@/store'
-import { fetchUser } from "@/store/auth";
-
+import { store } from "@/store";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function RootLayout({ children }) {
-  await store.dispatch(fetchUser())
-  // console.log(store.getState().user)
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Header/> */}
         <div className="flex">
-          <div className="w-1/6">
-            <Sidebar></Sidebar>
-          </div>
-          <div className="w-5/6">
-          <main>{children}</main>
-          </div>
+          <Sidebar/>
+          <main className="w-full">{children}</main>
         </div>
       </body>
     </html>
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import {
   faGear,
   faStore,
@@ -6,18 +7,28 @@ import {
   faQuestion,
   faDoorOpen,
   faUser,
+  faArrowLeft
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-import SidebarButton from "@/clientComponent/SidebarButton";
-
 const sidebar = () => {
-  const open = false
+  const [open,setOpen] = useState(false)
+  const toggleOpen = () =>{
+    setOpen(!open)
+  }
   return (
-    <div className={`${open ? "w-1/6" : "w-16" } h-screen fixed bg-slate-800`} >
+    <div
+      className={`${
+        open ? "sm:w-2/6 lg:w-1/6" : "w-16"
+      } h-screen fixed bg-slate-800 duration-300 `}
+    >
       <aside className="relative">
-        <SidebarButton open={open} />
-        <ul className="text-gray-100 p-4 list-none font-medium flex  flex-col gap-10">
+        <FontAwesomeIcon
+          className={`bg-white text-blue-950 text-xl rounded-full absolute -right-2 top-3 p-1 border border-blue-950 cursor-pointer ${!open && "rotate-180"}`}  
+          icon={faArrowLeft}
+          onClick={toggleOpen}
+        />
+        <ul className="text-gray-100 pt-5 list-none font-medium flex  flex-col gap-10">
           <li className=" cursor-pointer flex h-10 items-center px-4 hover:bg-slate-500 hover:text-white rounded-md">
             <FontAwesomeIcon icon={faUser} size="lg" />
             {/* <span className="ml-4">{user[0]?.fullname}</span> */}
@@ -25,30 +36,28 @@ const sidebar = () => {
           <li className=" cursor-pointer flex h-10 items-center px-4 hover:bg-slate-500 hover:text-white rounded-md">
             <Link href="/product">
               <FontAwesomeIcon icon={faStore} size="lg" />
-              <span className="ml-4">Products</span>
+              <span className={`${!open && "hidden" } ml-4`}>Products</span>
             </Link>
           </li>
 
           <li className=" cursor-pointer flex h-10 items-center px-4 hover:bg-slate-500 hover:text-white rounded-md">
             <Link href="/addProduct">
               <FontAwesomeIcon icon={faPlus} size="lg" />
-              <span className="ml-4">Add Product</span>
+              <span className={`${!open && "hidden" } ml-4`}>Add Product</span>
             </Link>
           </li>
 
           <li className="flex cursor-pointer h-10 items-center px-4 hover:bg-slate-500 hover:text-white rounded-md">
             <FontAwesomeIcon icon={faGear} size="lg" />
-            <span className="ml-4">Setting</span>
+            <span className={`${!open && "hidden" } ml-4`}>Setting</span>
           </li>
           <li className="flex cursor-pointer h-10 items-center px-4 hover:bg-slate-500 hover:text-white rounded-md">
             <FontAwesomeIcon icon={faQuestion} size="lg" />
-            <span className="ml-4">Help</span>
+            <span className={`${!open && "hidden" } ml-4`}>Help</span>
           </li>
           <li className="flex cursor-pointer h-10 items-center px-4 hover:bg-slate-500 hover:text-white rounded-md">
             <FontAwesomeIcon icon={faDoorOpen} size="lg" />
-            <button className="ml-4">
-              Logout
-            </button>
+            <button className={`${!open && "hidden" } ml-4`}>Logout</button>
           </li>
         </ul>
       </aside>
